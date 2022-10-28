b03e7ef731bcd854d57979fb96b8867f606d9c29
Andrey Tsaregorodtsev <andrey508@gmail.com>
2022-10-28 14:44:20 +0300

    Initial commit - base frontend(Home and ProductPage

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import { Container } from 'react-bootstrap'
+
+import Footer from './components/Footer'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Header from './components/Header'
+
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+
+
+function App() {
+  return (
+    <Router>
+        <Header/>
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route exact path='/' element={< HomePage />}></Route> 
+              <Route path='/product/:productId' element={< ProductPage />}></Route>
+            </Routes>
+          </Container>
+        </main>
+        <Footer/>
+    </Router>
+  );
+}
+
+export default App;
